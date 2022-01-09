ba08e88d9b34495e8ffdd671ad733c17e168682f
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-01-09 15:29:59 +0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Dmitry Sharabin on 09.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
